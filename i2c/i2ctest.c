b8755e9841a99a6d2ae9a9706a63ab1ce5a1b059
youngjun-park8197 <jjparkyj@gmail.com>
2021-05-20 16:50:31 +0900

    LEDcontrol & i2c source merged

--- i2c/i2ctest.c
+++ i2c/i2ctest.c
@@ -12,8 +12,13 @@ int main(int argc, char *argv[]) {
 	 * ---> argc : 4, argv = { "i2ctest", "1", "2", "3" }
 	 */
 
+	int chan = 4; // 전구 빛의 채널
+	int ret = wiringPiSetup();
+	pinMode(chan, OUTPUT);
+	printf("%d\n", ret);
+
 	float val;
-	int ch = atoi(argv[1]);
+	int sens = atoi(argv[1]);
 
 	if(argc < 2)  { printf("\nUsage : %s AIN_No \n\n", argv[0]); return 0; } // %s : 실행 파일명 (가장 첫번째 입력)
 	
@@ -21,28 +26,35 @@ int main(int argc, char *argv[]) {
 
 	if(strcmp(argv[1], "0") == 0) // argv[1]이 0일 때, ch = 0
 	{
-		ch = 0; 
+		sens = 0; 
 		printf("\n ********** 조도 센서 측정 ********** \n"); 
 	}
 	
 	if(strcmp(argv[1], "1") == 0) 
 	{ 
-		ch = 1;
+		sens = 1;
 		printf("\n ********** 온도 센서 측정 ********** \n"); 
 	}
 
 	if(strcmp(argv[1], "3") == 0) 
 	{ 
-		ch = 3;
+		sens = 3;
 		printf("\n **********  센서 측정 ********** \n"); 
 	}	
 
-	wiringPiI2CWrite(hndl, ch);
+	// 채널 초기화 작업(반복해서 2, 3바이트 정도 버림)
+	wiringPiI2CWrite(hndl, sens);
 	wiringPiI2CRead(hndl);
 
 	while(1) {
 		val = wiringPiI2CRead(hndl);
-		printf("read from I2C address [0] : %f\n", val);
+		
+		if(val >= 170) {
+			digitalWrite(chan, HIGH);
+		}
+		else digitalWrite(chan, LOW);
+
+		printf("read from I2C address [%d] : %f\n", sens, val);
 		delay(500);
 	}
 }
