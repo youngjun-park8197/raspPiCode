aa8d3421c712e02d5b9922baa921fc0205b7e4f2
youngjun-park8197 <jjparkyj@gmail.com>
2021-05-21 12:38:27 +0900

    i2c & LEDcontrol merged

--- LEDcontrol/LEDintense.c
+++ LEDcontrol/LEDintense.c
@@ -1,24 +1,34 @@
 // writedown for test raspberryPi contol
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <wiringPi.h>
+#include <wiringPiI2C.h>
 #include <softPwm.h>
 
+
 int main(int argc, char **argv) {
-	if(argc < 2) { printf("\nUsage : %s wpi-No\n\n", argv[0]); return 0; }
+	if(argc < 3) { printf("\nUsage : %s wpi-No\n\n", argv[0]); return 0; }
 	
 	int pinNo = atoi(argv[1]);
 	int pwmRange = 100;
-	
+	float val = 0.f;
+
 	wiringPiSetup();
 	pinMode(pinNo, OUTPUT); // 라즈베리파이의 메인보드 상에 핀 연결 번호 선택
-	softPwmCreate(pinNo, 0, pwmRange); // 
+	softPwmCreate(pinNo, 0, pwmRange); 
 
-	int check = 0;
+	// int check = 0;
+	int hndl = wiringPiI2CSetup(0x48);
+	
+	wiringPiI2CWrite(hndl, 0);
+	wiringPiI2CRead(hndl);
+
 	// 엔터키 입력 시 Light On / Off
 	while(1) {
-		getchar(); // 엔터키 입력
-		
+		val = wiringPiI2CRead(hndl);
+		// getchar(); // 엔터키 입력
+			
 		// 출력물 내보내기 : DigitalWrite( [핀번호], [신호수준] );
 		// * 출력수준 예시
 		// 5V ------------------------------------------- [ HIGH ] : VDD
@@ -31,12 +41,12 @@ int main(int argc, char **argv) {
 		 * compiler -> binary 
 		 * optimization : depends on run speed / code size ...
 		 */
-
-		if(check % 2 == 0)	
+		printf("\n outdoor Brightness value : %f \n", val);
+		if(val >= 200)	
 		{
-			for(int i = 0; i < pwmRange; i++) { // 속도 최적화 관점으로 전환 : 
+			for(int i = 0; i < pwmRange; i++) {
 				softPwmWrite(pinNo, i); // Dimming up
-				delay(50);
+				delay(15);
 			}
 		}
 
@@ -44,10 +54,9 @@ int main(int argc, char **argv) {
 		{
 			for(int i = pwmRange; i >= 0; i--) {
 				softPwmWrite(pinNo, i); // Dimming down
-				delay(30);
+				delay(15);
 			}
 		}	
-		check++;
 	}
 	return 0;
 }
