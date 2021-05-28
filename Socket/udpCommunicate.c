6a2f7ea14b3c454282ea9b4dba207b9956b4828e
youngjun-park8197 <jjparkyj@gmail.com>
2021-05-28 13:34:42 +0900

    sources editted

--- Socket/udpCommunicate.c
+++ Socket/udpCommunicate.c
@@ -7,29 +7,60 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <pthread.h>
 
-char* IP = "192.168.0.43";
-int PORT = 9200;
-struct sockaddr_in udp_info;
-int sock;
+void* readProc();
 
+int PORT = 9100;
+struct sockaddr_in sockinfo_sv, sockinfo_cli;
+int sock_sv, sock_cli;
+int f_cli = 0;
 
-int main() {
+int main() 
+{
 	char buf[512];
 
+	pthread_t readThread;
 	// 소켓 생성
-	sock = socket(AF_INET, SOCK_DGRAM, 0);
-	udp_info.sin_family = AF_INET;
-	inet_pton(AF_INET, IP, &udp_info.sin_addr.s_addr);
-	udp_info.sin_port = htons(PORT);
+	sock_sv = socket(AF_INET, SOCK_DGRAM, 0);
+	sock_cli = socket(AF_INET, SOCK_DGRAM, 0);
+	sockinfo_sv.sin_family = AF_INET;
+	sockinfo_sv.sin_addr.s_addr = htonl(INADDR_ANY);
+	sockinfo_sv.sin_port = htons(PORT);
+	
+	bind(sock_sv, (struct sockaddr*)&sockinfo_sv, sizeof(sockinfo_sv));
 
-	send(sock, buf, strlen(buf), 0);
+	pthread_create(&readThread, NULL, readProc, NULL); // thread 개시, 접속 요청이 있을 때에만 클라이언트 소켓에 대한 정보를 완성할 수 있음
 
-	while(1) {
-		printf("input text: ");
-		scanf("%s", buf);
-		if(buf[0] == 'q') break;
-		sendto(sock, buf, strlen(buf), 0, (struct sockaddr*)&udp_info, sizeof(udp_info));
+	printf("standing by remote message ... \n\n"); // 외부로부터의 접속을 기다린다는 문장을 출력
+
+	while(1) 
+	{
+		if(f_cli)
+		{	
+			printf("input text (0x%08x) : ");
+			scanf("%s", buf);
+			sockinfo_cli.sin_port = htons(PORT);
+			sendto(sock_cli, buf, strlen(buf), 0, (struct sockaddr*)&sockinfo_cli, sizeof(sockinfo_cli));
+		}
+		// delay(100);
 	}
 }
 
+
+void* readProc() 
+{
+	char buf[512];
+
+	while(1) 
+	{
+		int n = sizeof(sockinfo_cli);
+		int count = recvfrom(sock_sv, buf, 512, 0, (struct sockaddr*)&sockinfo_cli, &n);
+		if(count > 0) 
+		{
+			buf[count] = 0;
+			printf("%s\n", buf);
+			f_cli = 1;
+		}
+	}
+}
